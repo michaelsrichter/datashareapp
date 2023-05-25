f0f315dbe304e4bd0e98ed35590abad51f37bf1a
Mike Richter <michaelsrichter@hotmail.com>
2023-05-24 21:59:39 -0400

    update ama-script to be dynamic

--- ama-script.sh
+++ ama-script.sh
@@ -1,46 +1,58 @@
 
+randomstring=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
+echo $randomstring
+location=eastus
+rgname="amarg$randomstring"
+defrgname="amadefrg$randomstring"
+storagename="amastorage$randomstring"
+amaname="ama$randomstring"
+amadisplayname="Managed Data Share App $randomstring"
+containername=appcontainer
 
 zipfile=datashareapp.zip
 
+admingroup="AMA Admins Group"
 zip $zipfile createUiDefinition.json mainTemplate.json
 
-# az group create --name amarepository --location eastus
+# create resource group if not exists
+if [ $(az group exists --name $rgname) = false ]; then
+    az group create --name $rgname --location $location
+fi
 
 az storage account create \
-    --name amarepostorageaccount \
-    --resource-group amarepository \
-    --location eastus \
+    --name $storagename \
+    --resource-group $rgname \
+    --location $location \
     --sku Standard_LRS \
     --kind StorageV2
 
 az storage container create \
-    --account-name amarepostorageaccount \
-    --name appcontainer \
+    --account-name $storagename \
+    --name $containername \
     --public-access blob
 
 az storage blob upload \
-    --account-name amarepostorageaccount \
-    --container-name appcontainer \
+    --account-name $storagename \
+    --container-name $containername \
     --name "$zipfile" \
     --file "$zipfile" \
     --overwrite
 
-az group create --name amadefinition --location eastus
+if [ $(az group exists --name $defrgname) = false ]; then
+    az group create --name $defrgname --location $location
+fi
 
-groupid=$(az ad group show --group "AMA Admin Demo Group" --query id --output tsv)
+groupid=$(az ad group show --group "$admingroup" --query id --output tsv)
 ownerid=$(az role definition list --name Owner --query [].name --output tsv)
 
-
-
-
-blob=$(az storage blob url --account-name amarepostorageaccount --container-name appcontainer --name $zipfile --output tsv)
+blob=$(az storage blob url --account-name $storagename  --container-name $containername --name $zipfile --output tsv)
 
 az managedapp definition create \
-  --name "extractmanagedapp" \
-  --location "eastus" \
-  --resource-group amadefinition \
+  --name $amaname \
+  --location $location \
+  --resource-group $defrgname \
   --lock-level ReadOnly \
-  --display-name "Extract Managed Data Share App" \
-  --description "Data Share Solutions for Extract Customers" \
+  --display-name "$amadisplayname" \
+  --description "$amadisplayname" \
   --authorizations "$groupid:$ownerid" \
   --package-file-uri "$blob"
